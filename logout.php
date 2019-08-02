61c1b21c687bb56c3ad55f801dbde392efb49262
SirGladko <53437187+SirGladko@users.noreply.github.com>
2019-08-02 21:38:21 +0200

    Update logout.php
    eng

--- logout.php
+++ logout.php
@@ -1,9 +1,9 @@
 <?php
-    //podłączenie do bazy danych i podstawowe zmienne
+    //db connection and basic variables
     include('more/conf.php');
 
     if(isset($_POST["logout"])){
-        //wylogowanie
+        //logout
         $sql = "UPDATE users SET chat_used = NULL WHERE user_id = '$user_id';";
         $result = $conn->query($sql);
         $sql = "INSERT INTO records (user_id, chat_id, record_flag, text) values ('$user_id', '$chat_id', 3, '***$login OUT***');";
