f7038208073a63343dc04e176f40b603683f53be
SirGladko <53437187+SirGladko@users.noreply.github.com>
2019-08-03 17:24:07 +0200

    Update register.php
    eng

--- register.php
+++ register.php
@@ -67,7 +67,7 @@
                     <div class="login">
                         <form action="" method="POST">
                             <input class="logininput" id="input" name="newlogin" placeholder="Login" type="text" maxlength="<?php echo $login_len;?>" required autofocus>
-                            <input class="logininput" id="input" name="newpassw" placeholder="Hasło" type="password" maxlength="<?php echo $passw_len;?>" required>
+                            <input class="logininput" id="input" name="newpassw" placeholder="Password" type="password" maxlength="<?php echo $passw_len;?>" required>
                             <p class="error mg-b-20"><?php echo $registerErr ?></p>
                             <button class="btn" type="submit" name="register">Register</button>
                         </form>
