4703e9b096fce8c6e1a1c5a48568d1f573990f3a
SirGladko <53437187+SirGladko@users.noreply.github.com>
2019-08-03 16:48:45 +0200

    Update content.php
    eng

--- content.php
+++ content.php
@@ -1,8 +1,8 @@
 <?php
-    //podłączenie do bazy danych i podstawowe zmienne
+    //db connection and basic variables
     include('more/conf.php');
 
-    //jeżeli używane konto jest zbanowane to wyloguj
+    //if user is banned then log him out
     $sql = "SELECT user_flag from users where user_id='$user_id';";
     $result = $conn->query($sql);
     $row = $result->fetch_assoc();
@@ -11,28 +11,28 @@
         $_SESSION["ban"]=1;
         echo "<script>window.location.href = 'login.php' </script>";
     }
-    //sprawdzanie czasu ostatniej aktywnosci wszystkich użytkowników, którzy są przypisani jako aktywni na aktualnym czacie i ewentualne "wylogowanie" ich
+    //check last active date for every user that is assigned to your chat and log them out if its expired
     $sql = "SELECT user_id, user_flag, login, last_active FROM users WHERE chat_used='$chat_id';";
     $result = $conn->query($sql);
     while($row = $result->fetch_assoc()) {
-        //sprawdzenie czy nie jest nieaktywny dłużej niż timeout
+        //check if user was active recently
         if (time()-strtotime($row["last_active"])>$user_timeout && $row["user_flag"]!=2){
             $loguser_id = $row["user_id"];
             $loguser_login = $row["login"];
             $logdate = $row["last_active"];
-            //napisanie, że wyszedł z czatu, jeżeli nie jest aktywny
+            //write that user left the chat
             $sql1 = "INSERT INTO records (user_id, chat_id, record_flag, text, record_date) values ('$loguser_id', '$chat_id', 3, '***$loguser_login OUT***', '$logdate');";
             $result1 = $conn->query($sql1);
-            //usunięcie mu przypisania do czatu
+            //unbind him from the chat
             $sql1 = "UPDATE users SET chat_used = NULL WHERE user_id = '$loguser_id';";
             $result1 = $conn->query($sql1);
         }
     }
-    //sprawdzanie przypisania aktualnego użytkownika do aktualnego czatu
+    //check if you are assigned to your chat
     $sql = "SELECT chat_used, last_active FROM users WHERE user_id = '$user_id';";
     $result = $conn->query($sql);
     $row = $result->fetch_assoc();
-    //jeżeli nie jest do niego przypisany, to go przypisuje i wysyła wiadomość o dołączeniu
+    //if not, then assign and write "enter the chat" message
     if ($row["chat_used"]!=$chat_id){
         if (time()-strtotime($row["last_active"])>$logindate_reload){
             $sql = "UPDATE users SET chat_used = '$chat_id', date = current_timestamp WHERE user_id = '$user_id';";
@@ -44,30 +44,30 @@
         $sql = "INSERT INTO records (user_id, chat_id, record_flag, text) values ('$user_id', '$chat_id', 2, '***$login IN***');";
         $result = $conn->query($sql);
     }
-    //update czatu ostatniej aktywności
+    //update last active date
     $sql = "UPDATE users SET last_active = current_timestamp WHERE user_id = '$user_id';";
     $result = $conn->query($sql);
-    //wypisywanie wszystkich wpisów
+    //write chat content
     $sql = "SELECT record_id, login, user_flag, chat_name, record_flag, text, record_date FROM ((records INNER JOIN users ON records.user_id = users.user_id) INNER JOIN chats ON records.Chat_id = chats.chat_id) WHERE chat_name = '$chat' ORDER BY record_date;";
     $result = $conn->query($sql);
     while($row = $result->fetch_assoc()) {
-        //wyświetla tylko wiadomości do kilku minut przed zalogowaniem sie na czat (patrz config)
+        //write messages to few minutes before logging in (look in conf.php)
         if (strtotime($_SESSION["logindate"])<strtotime($row["record_date"])+$pre_login_chat){
-            //informacje do wyświetlania
+            //additional variables
             if ($_SESSION["permiss"]==1){
                 $wpis=" (".$row["record_id"]."):</p>";
             }
             else {
                 $wpis=":</p>";
             }
-            //kolorki userów
+            //nicknames colors
             if ($row["user_flag"]==1){
                 $recorduser="admin";
             }
             else {
                 $recorduser="user";
             }
-            //informacje o ostatnim wypisywanym użytkowniku
+            //it is because i didnt want to repeat nickname before messages if one person is writing few of them in a row
             if($last_printed_user==$row["login"]){
                 $last_printed="<div class=\"wpis\"><div class=\"wciecie\"><p>";
             }
@@ -75,48 +75,48 @@
                 $last_printed="<div class=\"wpis\"><p class=\"$recorduser\">".date('H:i', strtotime($row["record_date"]))." ".$row["login"].$wpis."<div class=\"wciecie\"><p>";
             }
             
-            //wyświetlanie
+            //echo messages
             if ($row["record_flag"]==0){
                 echo $last_printed.$row["text"]."</p></div></div>";
                 $last_printed_user=$row["login"];
             }
-            //info o dołączeniu
+            //info about logging in
             else if ($row["record_flag"]==2){
                 $last_printed_user="";
-                echo "<div class=\"login wpis\"><p class=\"$recorduser\">".$row["login"]."<p> dołączył do czatu</p></div>";
+                echo "<div class=\"login wpis\"><p class=\"$recorduser\">".$row["login"]."<p> entered the chat</p></div>";
             }
-            //info o opuszczeniu
+            //info about logging out
             else if ($row["record_flag"]==3){
                 $last_printed_user="";
-                echo "<div class=\"login wpis\"><p class=\"$recorduser\">".$row["login"]."<p> wyszedł z czatu</p></div>";
+                echo "<div class=\"login wpis\"><p class=\"$recorduser\">".$row["login"]."<p> left the chat</p></div>";
             }
-            //wiadomosci spersonalizowane
+            //personalized messages
             else if ($row["record_flag"]==4){
                 if($row["login"]==$login){
                     $last_printed_user="";
                     echo "<div class=\"personal wpis\"><p>".$row["text"]."</p></div>";
                 }
             }
-            //wiadomość o zablokowaniu i odblokowaniu użytkownika
+            //ban and unban messages
             else if ($row["record_flag"]==5){
                 $last_printed_user="";
                 echo "<div class=\"login wpis\"><p>".$row["text"]."</p></div>";
             }
-            //wpisy usunięte
+            //deleted records
             else if ($row["record_flag"]==6){
                 $last_printed_user="";
-                echo "<div class=\"wpis\"><p class=\"$recorduser\">".date('H:i', strtotime($row["record_date"]))." ".$row["login"].$wpis."<br><p class=\"wciecie\">Ten wpis został usunięty</p></div>";
+                echo "<div class=\"wpis\"><p class=\"$recorduser\">".date('H:i', strtotime($row["record_date"]))." ".$row["login"].$wpis."<br><p class=\"wciecie\">This message was deleted</p></div>";
             }
-            //wiadomosc o komendach
+            //command list
             else if ($row["record_flag"]==7){
                 if($row["login"]==$login){
                     $last_printed_user="";
                     echo "<div class=\"personal wpis\"><p></p><div class=class=\"personal\"><p>Spis Komend</p></div>
                     <div class=\"personal\"><p>/ban (nick)           -ban</p></div>
                     <div class=\"personal\"><p>/unban (nick)         -unban</p></div>
-                    <div class=\"personal\"><p>/del (nr. wpisu)      -usuwa wpis o podanym numerze</p></div>
-                    <div class=\"personal\"><p>/permiss (nick) (1/0) -ustawia uprawnienia użytkownika/admina</p></div>
-                    <div class=\"personal\"><p>/echo (nr. wpisu)     -pokazuje treść usuniętego wpisu</p></div><p></p></div>";
+                    <div class=\"personal\"><p>/del (record id)      -delete message with this id</p></div>
+                    <div class=\"personal\"><p>/permiss (nick) (1/0) -set user/admin permissions</p></div>
+                    <div class=\"personal\"><p>/echo (record id)     -show content of deleted message</p></div><p></p></div>";
                 }
             }
         }
